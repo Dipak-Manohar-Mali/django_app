6fa9fec7f804c4f342200c040a3f9b4c2f722c6b
Dipak-Manohar-Mali <dipakmali20003@gmail.com>
2023-11-17 20:32:10 +0530

    commit

---
+++ myproject/home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return HttpResponse("This is About Page")
+
+def services(request):
+    return HttpResponse("This is services Page")
+
+def contact(request):
+    return HttpResponse("This is contact Page")
