0b78d2e16c4af6120411190f68ec7d0ab39d292a
Dipak-Manohar-Mali <dipakmali20003@gmail.com>
2023-11-21 23:18:37 +0530

    commit

--- myproject/home/views.py
+++ myproject/home/views.py
@@ -5,10 +5,10 @@ def index(request):
     return render(request,'index.html')
 
 def about(request):
-    return HttpResponse("This is About Page")
+    return render(request,'about.html')
 
 def services(request):
-    return HttpResponse("This is services Page")
+    return render(request,'services.html')
 
 def contact(request):
-    return HttpResponse("This is contact Page")
+    return render(request,'contact.html')
